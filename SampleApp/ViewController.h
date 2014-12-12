9bd89393fc32e2050b85f2f72167db2496197fb0
Bhupender Gupta <bgupta@IM-PC-239.local>
2014-12-12 14:15:35 +0530

    Initial Commit

---
+++ SampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Bhupender Gupta on 12/12/14.
+//  Copyright (c) 2014 Bhupender Gupta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SampleApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SampleApp
+//
+//  Created by Bhupender Gupta on 12/12/14.
+//  Copyright (c) 2014 Bhupender Gupta. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
